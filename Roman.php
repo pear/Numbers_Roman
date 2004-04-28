0cbde6188ec87161d7669b616fc104b5bac77d49
Daniel Convissor <danielc@php.net>
2004-04-28 04:09:09 +0000

    Clean up coding standards. Easier to do it than to continue to explain it.
    git-svn-id: http://svn.php.net/repository/pear/packages/Numbers_Roman/trunk@157353 c90b9560-bf6c-de11-be94-00142212c4b1

--- Roman.php
+++ Roman.php
@@ -17,55 +17,51 @@
 // |            Sterling Hughes <sterling@php.net>                        |
 // +----------------------------------------------------------------------+
 // $Id$
-//
 
 // {{{ Numbers_Roman
 
 /**
-* The Numbers_Roman class provides utilities to convert roman numerals to
-* arabic numbers and convert arabic numbers to roman numerals
-* it supports lower case input and output and some furthers conversion
-* functions.
-*
-*
-* @access public
-* @author David Costa <gurugeek@php.net>
-* @author Sterling Hughes <sterling@php.net>
-* @package Numbers_Roman
-*/
-
+ * Provides utilities to convert roman numerals to
+ * arabic numbers and convert arabic numbers to roman numerals.
+ *
+ * Supports lower case input and output and some furthers conversion
+ * functions.
+ *
+ * @access public
+ * @author David Costa <gurugeek@php.net>
+ * @author Sterling Hughes <sterling@php.net>
+ * @package Numbers_Roman
+ */
 class Numbers_Roman
 {
     // {{{ toNumber()
 
     /**
-    * Converts a roman numeral to a number
-    *
-    * @param  string  $roman The roman numeral to convert
-    *                        lower cased numerals are converted into uppercase
-    * @return integer $num   The number corresponding to the
-    *                        given roman numeral
-    *
-    * @access public
-    */
-
+     * Converts a roman numeral to a number
+     *
+     * @param  string  $roman The roman numeral to convert
+     *                        lower cased numerals are converted into
+     *                        uppercase
+     * @return integer $num   The number corresponding to the
+     *                        given roman numeral
+     * @access public
+     */
     function toNumber($roman)
     {
         $roman = strtoupper($roman);
 
-    /*
-    * Replacing the Numerals representing an integer higher then 4000
-    * e.g. _X represent 10 000 _L  represent 50 000 etc
-    * we first convert them into single characters
-    *
-    */
-
+        /*
+         * Replacing the Numerals representing an integer higher then 4000
+         * e.g. _X represent 10 000 _L  represent 50 000 etc
+         * we first convert them into single characters
+         */
         $roman = str_replace("_V", "S", $roman);
         $roman = str_replace("_X", "R", $roman);
         $roman = str_replace("_L", "Q", $roman);
         $roman = str_replace("_C", "P", $roman);
         $roman = str_replace("_D", "O", $roman);
         $roman = str_replace("_M", "N", $roman);
+
         $conv = array(
             array("letter" => 'I', "number" => 1),
             array("letter" => 'V', "number" => 5),
@@ -82,6 +78,7 @@ class Numbers_Roman
             array("letter" => 'N', "number" => 1000000),
             array("letter" => 0, "number" => 0)
         );
+
         $arabic = 0;
         $state = 0;
         $sidx = 0;
@@ -108,44 +105,42 @@ class Numbers_Roman
         return $arabic;
     }
 
-        // }}}
-        // {{{ toRoman()
+    // }}}
+    // {{{ toRoman()
+
     /**
-    * A backwards compatibility alias for toNumeral()
-    *
-    * @access private
-    */
-    
+     * A backwards compatibility alias for toNumeral()
+     *
+     * @access private
+     */
     function toRoman($num, $uppercase = true)
     {
         return $this->toNumeral($num, $uppercase);
     }
 
-        // }}}
-        // {{{ toNumeral()
+    // }}}
+    // {{{ toNumeral()
 
     /**
-    * Converts a number to its roman numeral representation
-    *
-    * @param  integer $num         An integer between 0 and 3999 inclusive
-    *                              that should be converted to a roman numeral
-    *                              integers higher then 3999 are supported 
-    *                              from version 0.1.2
-    *           Note:
-    *           For an accurate result the integer shouldn't be higher then
-    *           5 999 999. Higher integers are still converted but they do not reflect an
-    *           historically correct Roman Numeral.
-    *
-    *                 $uppercase   Uppercase output: default true
-    *
-    *                 $html        Enable html overscore required for integers over 3999
-    *                              default true
-    * @return string  $roman The corresponding roman numeral
-    *
-    * @access public
-    *
-    */
-
+     * Converts a number to its roman numeral representation
+     *
+     * @param  integer $num         An integer between 0 and 3999
+     *                              inclusive that should be converted
+     *                              to a roman numeral integers higher than
+     *                              3999 are supported from version 0.1.2
+     *           Note:
+     *           For an accurate result the integer shouldn't be higher
+     *           than 5 999 999. Higher integers are still converted but
+     *           they do not reflect an historically correct Roman Numeral.
+     *
+     *                 $uppercase   Uppercase output: default true
+     *
+     *                 $html        Enable html overscore required for
+     *                              integers over 3999. default true
+     * @return string  $roman The corresponding roman numeral
+     *
+     * @access public
+     */
     function toNumeral($num, $uppercase = true, $html = true)
     {
         $conv = array(10 => array('X', 'C', 'M'),
@@ -185,76 +180,89 @@ class Numbers_Roman
                 }
             }
         }
-    /*
-    * Preparing the conversion of big integers over 3999
-    * One of the systems used by the Romans  to represent 4000 and bigger numbers
-    * was to add an overscore on the numerals. Because of the non ansi equivalent
-    * if the html output option is true we will return the overline in the html code
-    * if false we will return a _ to represent the overscore to convert from numeral to arabic
-    * we will always expect the _ as a representation of
-    * the html overscore
-    */
-    if ($html == true) {
-        $over = '<span style="text-decoration:overline;">';
-        $overe = '</span>';
-    } elseif ($html == false) {
-        $over = '_';
-        $overe = '';
+
+        /*
+         * Preparing the conversion of big integers over 3999.
+         * One of the systems used by the Romans  to represent 4000 and
+         * bigger numbers was to add an overscore on the numerals.
+         * Because of the non ansi equivalent if the html output option
+         * is true we will return the overline in the html code if false
+         * we will return a _ to represent the overscore to convert from
+         * numeral to arabic we will always expect the _ as a
+         * representation of the html overscore.
+         */
+        if ($html == true) {
+            $over = '<span style="text-decoration:overline;">';
+            $overe = '</span>';
+        } elseif ($html == false) {
+            $over = '_';
+            $overe = '';
+        }
+
+        /*
+         * Replacing the previously produced multiple MM with the
+         * relevant numeral e.g. for 1 000 000 the roman numeral is _M
+         * (overscore on the M) for 900 000 is _C_M (overscore on both
+         * the C and the M) We initially set the replace to AFS which
+         * will be later replaced with the M.
+         *
+         * 500 000 is   _D (overscore D) in Roman Numeral
+         * 400 000 is _C_D (overscore on both C and D) in Roman Numeral
+         * 100 000 is   _C (overscore C) in Roman Numeral
+         *  90 000 is _X_C (overscore on both X and C) in Roman Numeral
+         *  50 000 is   _L (overscore L) in Roman Numeral
+         *  40 000 is _X_L (overscore on both X and L) in Roman Numeral
+         *  10 000 is   _X (overscore X) in Roman Numeral
+         *   5 000 is   _V (overscore V) in Roman Numeral
+         *   4 000 is M _V (overscore on the V only) in Roman Numeral
+         *
+         * For an accurate result the integer shouldn't be higher then
+         * 5 999 999. Higher integers are still converted but they do not
+         * reflect an historically correct Roman Numeral.
+         */
+        $roman = str_replace(str_repeat("M", 1000),
+                             $over."AFS".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 900),
+                             $over."C".$overe.$over."AFS".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 500),
+                             $over."D".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 400),
+                             $over."C".$overe.$over."D".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 100),
+                             $over."C".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 90),
+                             $over."X".$overe.$over."C".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 50),
+                             $over."L".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 40),
+                             $over."X".$overe.$over."L".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 10),
+                             $over."X".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 5),
+                             $over."V".$overe, $roman);
+        $roman = str_replace(str_repeat("M", 4),
+                             "M".$over."V".$overe, $roman);
+
+        /*
+         * Replacing AFS with M used in both 1 000 000
+         * and 900 000
+         */
+        $roman = str_replace("AFS", "M", $roman);
+
+        /*
+         * Checking for lowercase output
+         */
+        if ($uppercase == false) {
+            $roman = strtolower($roman);
+        }
+
+        return $roman;
     }
-    /*
-    * Replacing the previously produced multiple MM with the relevant numeral
-    * e.g. for 1 000 000 the roman numeral is _M (overscore on the M)
-    * for 900 000 is _C_M (overscore on both the C and the M)
-    * We initially set the replace to AFS which will be later replaced with the M
-    *
-    * 500 000 is   _D (overscore D) in Roman Numeral
-    * 400 000 is _C_D (overscore on both C and D) in Roman Numeral
-    * 100 000 is   _C (overscore C) in Roman Numeral
-    *  90 000 is _X_C (overscore on both X and C) in Roman Numeral
-    *  50 000 is   _L (overscore L) in Roman Numeral
-    *  40 000 is _X_L (overscore on both X and L) in Roman Numeral
-    *  10 000 is   _X (overscore X) in Roman Numeral
-    *   5 000 is   _V (overscore V) in Roman Numeral
-    *   4 000 is M _V (overscore on the V only) in Roman Numeral
-    *
-    * For an accurate result the integer shouldn't be higher then
-    * 5 999 999. Higher integers are still converted but they do not reflect an
-    * historically correct Roman Numeral.
-    */
 
-    $roman = str_replace(str_repeat("M", 1000),  $over."AFS".  $overe, $roman);
-    $roman = str_replace(str_repeat("M", 900),   $over."C".$overe.$over."AFS". $overe, $roman);
-    $roman = str_replace(str_repeat("M", 500),   $over."D".    $overe, $roman);
-    $roman = str_replace(str_repeat("M", 400),   $over."C".$overe.$over."D".$overe, $roman);
-    $roman = str_replace(str_repeat("M", 100),   $over."C".    $overe, $roman);
-    $roman = str_replace(str_repeat("M", 90),    $over."X".$overe.$over."C".$overe, $roman);
-    $roman = str_replace(str_repeat("M", 50),    $over."L".    $overe, $roman);
-    $roman = str_replace(str_repeat("M", 40),    $over."X".$overe.$over."L".$overe, $roman);
-    $roman = str_replace(str_repeat("M", 10),    $over."X".    $overe, $roman);
-    $roman = str_replace(str_repeat("M", 5),     $over."V".    $overe, $roman);
-    $roman = str_replace(str_repeat("M", 4), "M".$over."V".    $overe, $roman);     
+    // }}}
 
-    /*
-    * Replacing AFS with M used in both 1 000 000
-    * and 900 000
-    *
-    */
-    $roman = str_replace("AFS", "M", $roman);
-
-    /*
-    *
-    * Checking for lowercase output
-    *
-    */
-    if ($uppercase == false) {
-        $roman = strtolower($roman);
-    }
-
-    return $roman;
-    }
-
-	// }}}
 }
+
 // }}}
 
 /*
@@ -263,4 +271,5 @@ class Numbers_Roman
  * c-basic-offset: 4
  * End:
  */
+
 ?>
